add263cbdb273130c3ae3a3ac4b51c7616a9e4e1
Anna Kerps <anya-kerps@bk.ru>
2022-06-23 22:38:22 +0300

    задача 2

---
+++ Homework/zadacha2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if( number1 > number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}");
+}
