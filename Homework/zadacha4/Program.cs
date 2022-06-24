8b2d10218152d276c09c3a3681ee24d511410140
Anna Kerps <anya-kerps@bk.ru>
2022-06-24 11:25:22 +0300

    задача 4

---
+++ Homework/zadacha4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите 3 числa: ");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+Console.WriteLine($"max = {max}");
